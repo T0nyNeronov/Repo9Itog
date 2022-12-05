21953c0bf839bd779a7baeb51c42737616ae6d42
Antony <ashout10@gmail.com>
2022-12-05 13:55:39 +0300

    Сумма натуральных элемтов в промежутке

---
+++ HW2/Program.cs
@@ -0,0 +1,31 @@
+﻿// Задайте значения M и N. 
+// Напишите программу, которая найдёт сумму натуральных
+// элементов в промежутке от M до N.
+// M = 1; N = 15 -> 120
+// M = 4; N = 8. -> 30
+
+
+Console.WriteLine("Введите начальное число N ");
+int n=Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите конечное число M ");
+int m =Convert.ToInt32(Console.ReadLine());
+
+
+int SumNum (int m)
+{
+    if (n==m)
+    {
+        return n;
+    }
+    else if (m>n)
+    {
+      return(m+SumNum(m-1));     
+    }
+     else
+     {
+       return(m+SumNum(m+1));      
+     }
+    
+}
+
+Console.WriteLine("Сумма натуральных элемтов в промежутке равна " +SumNum(m));
